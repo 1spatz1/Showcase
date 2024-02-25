551daf3f2513fc6102bec29493056035a169eb04
Spatzy <55000624+1spatz1@users.noreply.github.com>
2024-02-25 13:38:25 +0100

    new validation

--- Showcase.Application/Contact/Commands/ContactCommandValidator.cs
+++ Showcase.Application/Contact/Commands/ContactCommandValidator.cs
@@ -10,25 +10,31 @@ public class ContactCommandValidator : AbstractValidator<ContactCommand>
         RuleFor(x => x.FirstName)
             .NotEmpty()
             .MaximumLength(50)
-            .WithMessage("Subject must be less then 50 characters long.");
+            .WithMessage("Subject must be less then 60 characters long.");
         RuleFor(x => x.LastName)
             .NotEmpty()
             .MaximumLength(50)
-            .WithMessage("Subject must be less then 50 characters long.");
+            .WithMessage("Subject must be less then 60 characters long.");
         RuleFor(x => x.PhoneNumber)
             .NotEmpty()
-            .Matches(@"\d{10}");
+            .Matches(@"(^\+[0-9]{2}|^\+[0-9]{2}\(0\)|^\(\+[0-9]{2}\)\(0\)|^00[0-9]{2}|^0)([0-9]{9}$|[0-9\-\s]{10}$)")
+            .WithMessage("Subject must be a phone number.")
+            .MaximumLength(20)
+            .WithMessage("Subject must be less then 20 characters long.");
         RuleFor(x => x.Email)
             .NotEmpty()
-            .EmailAddress();
+            .EmailAddress()
+            .WithMessage("Subject must be an email address.")
+            .MaximumLength(80)
+            .WithMessage("Subject must be less then 80 characters long.");
         RuleFor(x => x.Subject)
             .NotEmpty()
             .MaximumLength(200)
-            .WithMessage("Subject must be less then 600 characters long.");
+            .WithMessage("Subject must be less then 200 characters long.");
         RuleFor(x => x.Message)
             .NotEmpty()
-            .MaximumLength(600)
-            .WithMessage("Message must be less then 600 characters long.");
+            .MaximumLength(2000)
+            .WithMessage("Message must be less then 2000 characters long.");
         RuleFor(x => x.RecaptchaToken)
             .NotEmpty();
     }
