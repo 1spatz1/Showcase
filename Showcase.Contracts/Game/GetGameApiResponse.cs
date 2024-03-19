956041de54904760d3f94c7cd54cf65660d64d85
Spatzy <55000624+1spatz1@users.noreply.github.com>
2024-03-19 11:46:12 +0100

    another smoll fix

--- Showcase.Contracts/Game/GetGameApiResponse.cs
+++ Showcase.Contracts/Game/GetGameApiResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace Showcase.Contracts.Game;
 
-public record GetGameApiResponse(Guid UserId, string UserName, string Token, Domain.Entities.Game Game)
+public record GetGameApiResponse(Guid UserId, string Username, Domain.Entities.Game Game)
 {
     
 }
