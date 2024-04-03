f29e86b01b7ca7f71e3cfa91bb83cb7422e51757
Spatzy <55000624+1spatz1@users.noreply.github.com>
2024-04-03 14:50:04 +0200

    betere wachtwoorden

--- Showcase.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
+++ Showcase.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -17,8 +17,8 @@ public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
             .WithMessage("Username must be less than 30 characters long.");
         RuleFor(x => x.Password)
             .MaximumLength(128)
-            .WithMessage("Password must be less than 80 characters long.")
-            // Must have digit
+            .WithMessage("Password must be less than 128 characters long.")
+            /*// Must have digit
             .Matches(@"[0-9]+")
             .WithMessage("Password must contain at least one digit.")
             // Must have lowercase
@@ -29,14 +29,14 @@ public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
             .WithMessage("Password must contain at least one uppercase letter.")
             // Must have non-alphanumeric
             .Matches(@"\W+")
-            .WithMessage("Password must contain at least one non-alphanumeric character.")
-            // Must be at least 8 characters
-            .MinimumLength(8)
-            .WithMessage("Password must be at least 8 characters long.");
+            .WithMessage("Password must contain at least one non-alphanumeric character.")*/
+            // Must be at least 14 characters
+            .MinimumLength(14)
+            .WithMessage("Password must be at least 14 characters long.");
         RuleFor(x => x.ConfirmPassword)
             .MaximumLength(128)
             .WithMessage("ConfirmPassword must be less than 128 characters long.")
-            .MinimumLength(8)
-            .WithMessage("ConfirmPassword must be at least 8 characters long.");
+            .MinimumLength(14)
+            .WithMessage("ConfirmPassword must be at least 14 characters long.");
     }
 }
